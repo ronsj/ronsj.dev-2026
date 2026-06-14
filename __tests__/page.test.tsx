589dd335ccfd43ada5884ebf0c21a33fc93c4adb
Ronald San Jose <ronsj1@gmail.com>
2026-06-13 23:07:21 -0700

    Remove ThemeProvider from page tests

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,14 +1,9 @@
 import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import { ThemeProvider } from '../components/theme-provider'
 import Page from '../app/page'
 
 function renderPage() {
-  return render(
-    <ThemeProvider initialTheme="dark">
-      <Page />
-    </ThemeProvider>
-  )
+  return render(<Page />)
 }
 
 test('home page renders hero heading', () => {
